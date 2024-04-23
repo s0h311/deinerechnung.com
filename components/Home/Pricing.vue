0e957560bc269d9f3f0a85c54ea5b5c679a5fcb0
Soheil <soheil.nazari@check24.de>
2024-04-23 12:39:06 +0200

    add subscription option

--- components/Home/Pricing.vue
+++ components/Home/Pricing.vue
@@ -5,34 +5,88 @@
   >
     <h2 class="text-3xl text-center">Preise</h2>
 
-    <div class="border border-accent p-8 rounded-xl space-y-10">
-      <p class="line-through text-sm -mb-8">69,99€</p>
-      <div class="flex items-center gap-2">
-        <p class="text-3xl font-semibold">39,99€</p>
-        <p class="text-sm">/ einmalig</p>
-      </div>
-
-      <ul>
-        <li
-          v-for="(benefit, index) in benefits"
-          :key="'benefit' + index"
-          class="flex items-center gap-2 text-sm"
-        >
-          <IconCheck /> {{ benefit }}
-        </li>
-      </ul>
-
-      <NuxtLink
-        :to="STRIPE_LINK"
-        class="btn btn-primary w-full"
-        >Jetzt holen</NuxtLink
+    <div class="grid grid-cols-2 gap-20">
+      <div
+        v-for="(option, index) in pricingOptions"
+        :key="'option' + index"
+        class="border-2 p-8 rounded-xl space-y-10 shadow-md"
+        :class="option.isPremium ? 'border-primary' : 'border-neutral'"
       >
+        <p
+          class="line-through text-sm -mb-8"
+          :class="option.oldPrice ? '' : 'invisible'"
+        >
+          {{ option.oldPrice }}€
+        </p>
+        <div class="flex items-center gap-2">
+          <p class="text-3xl font-semibold">{{ option.newPrice }}€</p>
+          <p class="text-sm">/ {{ option.paymentPeriodText }}</p>
+        </div>
+
+        <ul>
+          <li
+            v-for="(benefit, index) in option.benefits"
+            :key="'benefit' + index"
+            class="flex items-center gap-2 text-sm"
+          >
+            <IconCheck /> {{ benefit }}
+          </li>
+        </ul>
+
+        <NuxtLink
+          class="btn w-full"
+          :class="option.isPremium ? 'btn-primary' : 'btn-neutral'"
+          @click="handleGoToCheckout(index)"
+        >
+          <span
+            v-if="isLoading === index"
+            class="loading loading-spinner"
+          />
+
+          <p v-else>Jetzt holen</p>
+        </NuxtLink>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const benefits = ['Automatische Verbesserungen', '24/7 Kundenservice', 'Einmal zahlen, für immer benutzen']
+const isLoading = ref<number | null>(null)
 
-const STRIPE_LINK = 'https://buy.stripe.com/test_00geWo2MG2Pu9jO9AA'
+const pricingOptions = [
+  {
+    title: 'Monatlich',
+    oldPrice: 9.99,
+    newPrice: 7.99,
+    paymentPeriod: 'monthly',
+    paymentPeriodText: 'monatlich',
+    benefits: ['Automatische Verbesserungen', '24/7 Kundenservice', 'Flexible monatlich zahlen'],
+    isPremium: false,
+  },
+
+  {
+    title: 'Einmalig, für immer',
+    oldPrice: 69.99,
+    newPrice: 39.99,
+    paymentPeriod: 'oneTime',
+    paymentPeriodText: 'einmalig',
+    benefits: ['Automatische Verbesserungen', '24/7 Kundenservice', 'Einmal zahlen, für immer benutzen'],
+    isPremium: true,
+  },
+]
+
+async function handleGoToCheckout(pricingOptionIndex: number): Promise<void> {
+  isLoading.value = pricingOptionIndex
+
+  const stripeCheckoutUrl = await $fetch('/api/stripe/checkout', {
+    method: 'post',
+    body: {
+      paymentPeriod: pricingOptions[pricingOptionIndex].paymentPeriod,
+    },
+  })
+
+  isLoading.value = null
+
+  await navigateTo(stripeCheckoutUrl, { external: true })
+}
 </script>
---
+++ pages/checkout/success.vue
@@ -0,0 +1,5 @@
+<template>
+  <p>success</p>
+</template>
+
+<script setup lang="ts"></script>
