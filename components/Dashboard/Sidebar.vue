30b18838e2b0540fce23c8806a77321504cb4308
Soheil <kontakt@soheilnazari.de>
2024-05-04 22:26:04 +0200

    add UISidebar component

--- components/Dashboard/Sidebar.vue
+++ components/Dashboard/Sidebar.vue
@@ -1,38 +1,12 @@
 <template>
-  <aside class="flex flex-col gap-5 p-10 border-r h-screen w-fit">
-    <NuxtLink
-      v-for="{ title, path } in links"
-      :key="path"
-      class="px-3 py-2 rounded-lg"
-      :class="route.path === path ? 'bg-neutral text-base-200' : 'hover:bg-base-200'"
-      :to="path"
-    >
-      {{ title }}
-    </NuxtLink>
-
-    <div class="grid justify-items-start gap-2 text-sm mt-auto">
-      <NuxtLink
-        v-for="{ title, path } in bottomLinks"
-        :key="path"
-        class="px-3 py-2 rounded-lg"
-        :class="route.path === path ? 'bg-neutral text-base-200' : 'hover:bg-base-200'"
-        :to="path"
-      >
-        {{ title }}
-      </NuxtLink>
-      <button
-        class="px-3 py-2 rounded-lg hover:bg-base-200"
-        @click="handleSignout"
-      >
-        Abmelden
-      </button>
-    </div>
-  </aside>
+  <UISidebar
+    :links="links"
+    :bottom-links="bottomLinks"
+    :bottom-btns="bottomBtns"
+  />
 </template>
 
 <script setup lang="ts">
-const route = useRoute()
-
 const supabase = useSupabaseClient()
 const sender = await useSender()
 
@@ -47,6 +21,8 @@ const bottomLinks = [
   { title: 'Rechnungeinstellungen', path: '/dashboard/invoiceSettings' },
   { title: 'Kontoeinstellungen', path: '/dashboard/accountSettings' },
 ]
+
+const bottomBtns = [{ title: 'Abmelden', handleFn: handleSignout }]
 
 async function handleSignout(): Promise<void> {
   await supabase.auth.signOut()
